933dfa411a5afde5572d83973611e9b7ae39d4b4
Hadi Sharghi <hsharghi@gmail.com>
2019-05-08 12:06:34 +0430

    add zip to document category

--- src/components/file.vue
+++ src/components/file.vue
@@ -98,7 +98,7 @@
           image: ['gif', 'jpg', 'jpeg', 'png', 'bmp', 'webp'],
           video: ['mp4', 'm3u8', 'rmvb', 'avi', 'swf', '3gp', 'mkv', 'flv'],
           audio: ['mp3', 'wav', 'wma', 'ogg', 'aac', 'flac'],
-          document: ['doc', 'txt', 'docx', 'pages', 'epub', 'pdf', 'numbers', 'csv', 'xls', 'xlsx', 'keynote', 'ppt', 'pptx']
+          document: ['doc', 'txt', 'docx', 'pages', 'epub', 'pdf', 'numbers', 'csv', 'xls', 'xlsx', 'keynote', 'ppt', 'pptx', 'zip']
         }
         Object.keys(typeMap).forEach((_type) => {
           const extensions = typeMap[_type]
